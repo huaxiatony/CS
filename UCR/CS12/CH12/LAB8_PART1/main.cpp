c3f3473c26823112065514f9831dcb70862d09cf
lootryx <looteraly@gmail.com>
2017-05-23 10:49:05 -0700

    Edited
    Put in 2 possible methods this time, instead of just one that does it
    recursively (non-recursive method excluded in the count).

--- UCR/CS12/CH12/LAB8_PART1/main.cpp
+++ UCR/CS12/CH12/LAB8_PART1/main.cpp
@@ -23,10 +23,16 @@ void flipString(string &s){                         //Reverses a string recursiv
         return;
     else{
         /*s = string(s.rbegin(), s.rend()); */      //Non-recursive way
-        char letter = s.at(s.size() - 1);           //This method cuts off the last character of the string
-        string newS = s.substr(0, s.size() - 1);    //And then adds it to the front of the string recursively
-        flipString(newS);                           //
-        s = letter + newS;
+        
+        //Method 01
+        /*string newS = s.substr(0, s.size() - 1);  //This method cuts off the last character of the string
+        flipString(newS);                           //And then adds it to the front of the string recursively
+        s = s.at(s.size() - 1) + newS;*/
+        
+        //Method 02
+        string text = s.substr(1, s.size());        //This method cuts off the first character of the string
+        flipString(text);                           //And then adds it to the back of the string recursively
+        s = text + s.at(0);
     }
     return;
 }
